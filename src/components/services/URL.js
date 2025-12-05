0d0a5b7cf08623021ae1d419a9ef1c339e55d173
SahilPethani <sahilpethani50@gmail.com>
2025-12-05 09:11:40 +0530

    url change commit

--- src/components/services/URL.js
+++ src/components/services/URL.js
@@ -1,1 +1,2 @@
-export const baseURL = `http://localhost:7000/api/v1/`;
+export const baseURL = `https://collegeproject-backend-r6v3.onrender.com/api/v1/`;
+// export const baseURL = `http://localhost:7000/api/v1/`;
